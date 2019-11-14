02be01619e0018b1dfb37fc00bf2181d9d883438
raphaelrpl <raphael.wcosta@gmail.com>
2019-11-14 11:17:40 -0200

    set model fields

--- bdc_scripts/models/catalog/qlook.py
+++ bdc_scripts/models/catalog/qlook.py
@@ -6,5 +6,5 @@ class Qlook(BaseModel):
     __tablename__ = 'Qlook'
     __table_args__ = dict(schema='catalogo')
 
-    sceneid = Column('SceneId', String(64), primary_key=True, index=True)
-    filename = Column('QLfilename', String(255), nullable=False, unique=True)
+    SceneId = Column('SceneId', String(64), primary_key=True, index=True)
+    QLfilename = Column('QLfilename', String(255), nullable=False, unique=True)

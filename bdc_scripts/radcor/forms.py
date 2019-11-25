bac7c97ccee1f6a86ed1c6ccd08d5c7f5d7bf127
raphaelrpl <raphael.wcosta@gmail.com>
2019-11-24 23:31:37 -0300

    Set forms to list activity history

--- bdc_scripts/radcor/forms.py
+++ bdc_scripts/radcor/forms.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, fields
 from marshmallow_sqlalchemy import ModelSchema
 from bdc_scripts.models import db
-from bdc_scripts.radcor.models import RadcorActivity
+from bdc_scripts.radcor.models import RadcorActivity, RadcorActivityHistory
 
 
 class TaskSchema(Schema):
@@ -12,8 +12,23 @@ class TaskSchema(Schema):
     traceback = fields.Str()
 
 
+class HistoryForm(ModelSchema):
+    task_status = fields.Method('dump_status')
+
+    def dump_status(self, obj):
+        return obj.task.status
+
+    class Meta:
+        model = RadcorActivity
+        sqla_session = db.session
+    # start_date = fields.DateTime()
+    # end_date = fields.DateTime()
+
+    # task = fields.Nested(TaskSchema, many=False)
+
+
 class RadcorActivityForm(ModelSchema):
-    task = fields.Nested(TaskSchema, many=False)
+    history = fields.Nested(HistoryForm, many=True)
 
     class Meta:
         model = RadcorActivity

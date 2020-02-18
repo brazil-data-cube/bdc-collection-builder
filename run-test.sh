96341867561a6d3bfb1888f9c59a23fbeab14415
raphaelrpl <raphael.wcosta@gmail.com>
2020-02-18 08:51:06 -0300

    Sphinx static

--- run-test.sh
+++ run-test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
-# This file is part of BDC Collection Builder.
+# This file is part of Brazil Data Cube Collection Builder.
 # Copyright (C) 2019 INPE.
 #
-# BDC Collection Builder is free software; you can redistribute it and/or modify it
+# Brazil Data Cube Collection Builder is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
